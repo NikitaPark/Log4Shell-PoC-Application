8368ab150b9e73427ee1a9d47613fb4cd8bbce85
NikiaPark <auddn33354@gmail.com>
2024-05-30 17:11:35 +0900

    Update: Create Home Control view

--- src/main/java/com/nikitapark/log4shellpocapplication/controller/HomeController.java
+++ src/main/java/com/nikitapark/log4shellpocapplication/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.nikitapark.log4shellpocapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
